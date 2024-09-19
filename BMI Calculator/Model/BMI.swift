1db8ae358a5fa9db14ef69af3b71489d77a08682
Dip Dutt <emduttt456@gmail.com>
2024-09-19 18:28:32 +0600

    [Feature] add three properties in BMI Model File

---
+++ BMI Calculator/Model/BMI.swift
@@ -0,0 +1,17 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Dip Dutt on 19/9/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct BMI {
+    let value:Float
+    let advice:String
+    let color:UIColor
+}
+
